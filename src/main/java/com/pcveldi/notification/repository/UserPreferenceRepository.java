928cd7d9688e87b201ed903fe70d7b2c9da03f75
Pc Veldi <65594452+PcVeldi22@users.noreply.github.com>
2026-07-06 14:06:27 -0400

    Create UserPreferenceRepository interface

---
+++ src/main/java/com/pcveldi/notification/repository/UserPreferenceRepository.java
@@ -0,0 +1,9 @@
+package com.pcveldi.notification.repository;
+
+import com.pcveldi.notification.model.UserPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserPreferenceRepository extends JpaRepository<UserPreference, String> {
+}
